211adb72ffdc00081ccc3d532d2603ddac65982e
Stevnbak <kasper@stevnbak.dk>
2023-06-14 22:54:07 +0200

    Backend server settings

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,7 @@
+//Debug vs production settings
+const enableHttps = false;
+const frontendPath = "../frontend/dist";
+
 import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
@@ -5,64 +9,75 @@ import rateLimit from "express-rate-limit";
 import cors from "cors";
 import compression from "compression";
 
-//Setup server settings
+//Setup express server with settings
 export const app = express();
-const limiter = rateLimit({
-	windowMs: 1000, // 1 second
-	max: 100 // limit each IP to 100 requests per windowMs
-});
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(helmet());
-app.use(limiter);
+app.use(
+	rateLimit({
+		windowMs: 1000, // 1 second
+		max: 100 // limit each IP to 100 requests per windowMs
+	})
+);
 app.use(express.json());
 app.use(cors());
 app.use(compression());
 
 //Function to run on all requests
 app.use(function (req, res, next) {
-	//Content Security Policy
-	res.setHeader("Content-Security-Policy", "default-src 'self';" + "img-src 'self' data: thecyclefrontier.wiki;" + "script-src 'self' 'sha256-reBsRZd5I88opZSwT59Ir+QlBhrEhdRJ1aQUr4GXhyw=';" + "style-src 'self' 'unsafe-inline' fonts.googleapis.com;" + "font-src 'self' fonts.gstatic.com;" + "connect-src 'self' raw.githubusercontent.com/TCF-Wiki/TCF-Information/;");
+	//Set Content Security Policy (CSP)
+	res.setHeader("Content-Security-Policy", "default-src 'self';" + "img-src 'self' data: thecyclefrontier.wiki;" + "script-src 'self' 'sha256-reBsRZd5I88opZSwT59Ir+QlBhrEhdRJ1aQUr4GXhyw=';" + "style-src 'self' 'unsafe-inline' fonts.googleapis.com;" + "font-src 'self' data: fonts.gstatic.com;" + "connect-src 'self' raw.githubusercontent.com/TCF-Wiki/TCF-Information/;");
 	//Redirect http to https
-	if (req.hostname != "localhost" && req.hostname != "127.0.0.1" && !req.secure) {
-		return res.redirect("https://" + req.headers.host + req.url);
+	if (enableHttps) {
+		if (!req.secure) {
+			return res.redirect("https://" + req.headers.host + req.url);
+		}
 	}
 	next();
 });
 
 //Static files
-app.use(express.static("../frontend/dist"));
+app.use(express.static(frontendPath));
 
-//Setup http & https server
+//Socket.io
+import {Server as IOServer} from "socket.io";
+let ioServer: IOServer;
+
+//Setup http server
 import http from "http";
-import https from "https";
-
-// Certificate
-import fs from "fs";
-/**const privateKey = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/privkey.pem", "utf8");
-const certificate = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/cert.pem", "utf8");
-const ca = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/chain.pem", "utf8");
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};*/
-
-//Start server
-///const httpsServer = https.createServer(credentials, app);
 const httpServer = http.createServer(app);
-
-/*httpsServer.listen(443, () => {
-	console.log("Server listening to port 443 (HTTPS)");
-});*/
-
-httpServer.listen(5000, () => {
-	console.log("Server listening to port 5000 (HTTP)");
+httpServer.listen(80, () => {
+	console.log("Server listening to port 80 (HTTP)");
 });
 
-//Setup socket.io
-import {Server as IOServer} from "socket.io";
-export const io = new IOServer(httpServer);
+//Setup https server
+import fs from "fs";
+import https from "https";
 
+if (enableHttps) {
+	//Certificate
+	const privateKey = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/privkey.pem", "utf8");
+	const certificate = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/cert.pem", "utf8");
+	const ca = fs.readFileSync("/etc/letsencrypt/live/games.thecyclefrontier.wiki/chain.pem", "utf8");
+	const credentials = {
+		key: privateKey,
+		cert: certificate,
+		ca: ca
+	};
+
+	//Start server
+	const httpsServer = https.createServer(credentials, app);
+	httpsServer.listen(443, () => {
+		console.log("Server listening to port 443 (HTTPS)");
+	});
+	//Setup socket.io
+	ioServer = new IOServer(httpsServer);
+} else {
+	ioServer = new IOServer(httpServer);
+}
+export const io = ioServer;
+
+//Stats
 setInterval(function () {
 	console.log("Amount of connected sockets is " + io.engine.clientsCount);
 	console.log("Currently open rooms:", io.sockets.adapter.rooms);
@@ -86,4 +101,4 @@ for (const file of logicFiles) {
 }
 
 //Redirect everything else to dist
-app.use("/*", express.static("../frontend/dist"));
+app.use("/*", express.static(frontendPath));
